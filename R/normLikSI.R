f8cf2f399e7d506820c57800d75b2b10e5e61de0
Derek Smith <derek.smith@vanderbilt.edu>
2017-02-06 20:25:52 +0000

    version 1.1

--- R/normLikSI.R
+++ R/normLikSI.R
@@ -1,12 +1,19 @@
 #' @export
 #' @import stats
-normLikSI <- function(dat, level, tol=.001, conf=F){
-  pl <- function(a) {sapply(a, function(y) mean((dat-y)^2)^(-length(dat)/2))/mean((dat-mean(dat))^2)^(-length(dat)/2)-1/level}
-  llim <- uniroot(pl, c(min(dat)-2*sd(dat), mean(dat)), tol=tol)$root
-  ulim <- uniroot(pl, c(mean(dat), max(dat)+2*sd(dat)), tol=tol)$root
+normLikSI <- function(dat, level, tol=.001, conf=F, B=500){
+  pl <- function(z) sapply(1:length(z), function(w) sum(dnorm(dat, z[w], sqrt(sum((dat-z[w])^2)/length(dat)), log=T)))
+  pl2 <- function(z) exp(pl(z)-pl(mean(dat)))-1/8
+  llim <- uniroot(pl2, c(mean(dat)-2*sd(dat), mean(dat)), tol=tol)$root
+  ulim <- uniroot(pl2, c(mean(dat), mean(dat)+2*sd(dat)), tol=tol)$root
   if(conf==F) {return(c(llim, ulim))
   } else{
-    out <- list(c(llim, ulim), pchisq(-2*log(1/level),1))
+    covs <- 0
+    for(i in 1:B){
+      bdat <- rnorm(length(dat), mean(dat), sd(dat))
+      bsi <- normLikSI(bdat, level)
+      if(bsi[1]<mean(dat) & bsi[2]> mean(dat)){covs <- covs+1}
+    }
+    out <- list(c(llim, ulim), covs/B)
     names(out) <- c("si", "conf.equiv")
     return(out)
   }
